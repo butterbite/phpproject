09634e561418bc4e494ee61ba23190dfabf2a9cf
root <root@localhost.localdomain>
2020-04-14 00:09:35 +0530

    Added home.php

---
+++ home.php
@@ -0,0 +1,39 @@
+<?php include('server.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Registration</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="header">
+          <h2>Register</h2>
+    </div>
+    <form method="POST" action="register.php">
+        <!-- display validation error here-->
+        <?php include('errors.php'); ?>
+        <div class="input-group">
+             <label>UserName</label>
+             <input type="text" name="username" value="<?php echo $username; ?>">
+        </div>
+        <div class="input-group">
+             <label>Email</label>
+             <input type="text" name="email" value="<?php echo $email; ?>">
+        </div>
+        <div class="input-group">
+             <label>Password</label>
+             <input type="password" name="password_1">
+        </div>
+        <div class="input-group">
+             <label>Confirm Password</label>
+             <input type="password" name="confirm password_2">
+        </div>
+        <div class="input-group">
+             <button type="submit" name="register" class="btn">Register</button>
+        </div>
+        <p>
+           Already a member? <a href="login.php">Sign In</a>
+        </p>
+    </form>
+</body>
+</html>
